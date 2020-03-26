d3713d29bb73a6a04f80c59bbe22b9a9fc43200e
vvilche1 <48952733+vvilche1@users.noreply.github.com>
2020-03-26 19:03:44 -0300

    Victor
    Agrege que la camara siguiera la pelota

---
+++ Assets/SeguirPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirPlayer : MonoBehaviour
+{
+    public Transform seguido;
+    public Vector3 vectorDiferencia;
+    public Vector3 vectorDiferenciaAngulos;
+
+
+
+    void Start()
+    {
+        vectorDiferencia = seguido.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = seguido.position - vectorDiferencia;
+    }
+}
--- Assets/move.cs
+++ Assets/move.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class move : MonoBehaviour{
     public float speed;
     private Rigidbody rigid;
+    public int vida = 100;
+    public int puntos = 0;
+   
 
     // Start is called before the first frame update
     void Start(){
