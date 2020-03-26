4f1c91d5f67532b8cd4df5ac8863f9fb4e50c894
Creinosoc <Creinosoc@ing.ucsc.cl>
2020-03-26 11:18:55 -0300

    Pelota
    agrege la pelota

---
+++ Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour{
+    public float speed;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start(){
+        rigid = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+
+        float Horizontal = Input.GetAxis("Horizantal");
+        float Veritical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(Horizontal, 0.0f , Veritical);
+
+        rigid.AddForce(move * speed);
+
+    }
+}
