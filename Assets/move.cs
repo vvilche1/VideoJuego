a547b98183f4c4db02a9ed89f7965818587f18cc
vvilche1 <48952733+vvilche1@users.noreply.github.com>
2020-03-28 22:15:48 -0300

    victor
    pelota

--- Assets/move.cs
+++ Assets/move.cs
@@ -3,26 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class move : MonoBehaviour{
-    public float speed;
-    private Rigidbody rigid;
+
     public int vida = 100;
     public int puntos = 0;
-   
+    public float velocidad = 1f;
+    [Range(10f, 30f)]
+    public float fuerzaSalto;
+
+
+    //public float speed;
+    //private Rigidbody rigid;
+    //public int vida = 100;
+    //public int puntos = 0;
+
 
     // Start is called before the first frame update
     void Start(){
-        rigid = GetComponent<Rigidbody>();   
+       // rigid = GetComponent<Rigidbody>();   
     }
+
+    //// Update is called once per frame
+    //void Update(){
+
+    //    float Horizontal = Input.GetAxis("Horizantal");
+    //    float Veritical = Input.GetAxis("Vertical");
+
+    //    Vector3 move = new Vector3(Horizontal, 0.0f , Veritical);
+
+    //    rigid.AddForce(move * speed);
+
+    //}
 
     // Update is called once per frame
-    void FixedUpdate(){
-
-        float Horizontal = Input.GetAxis("Horizantal");
-        float Veritical = Input.GetAxis("Vertical");
-
-        Vector3 move = new Vector3(Horizontal, 0.0f , Veritical);
-
-        rigid.AddForce(move * speed);
-
+    void Update()
+    {
+        if (this.vida > 0)
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(velocidad * Input.GetAxis("Horizontal"), (Input.GetButtonDown("Jump") && esNivelSuelo == true ? fuerzaSalto * 15 : 0f), velocidad * Input.GetAxis("Vertical")));
     }
+
+    public bool esNivelSuelo = true;
+
+
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "suelo")
+        {
+            Debug.Log("CHOCANDO CON SUELO");
+            esNivelSuelo = true;
+        }
+        else if (collision.gameObject.tag != "suelo")
+        {
+            Debug.Log("NO ENCUENTRO EL SUELO!");
+            esNivelSuelo = false;
+        }
+
+        if (collision.gameObject.tag == "lava")
+        {
+            Debug.Log("Quemandome en la lava");
+                esNivelSuelo = true;
+            //if (this.vida > 0) {
+            //    this.vida -= collision.gameObject.GetComponent<Lava>().daño;
+            //}
+
+        } else if (collision.gameObject.tag != "lava")
+        {
+            Debug.Log("Quemandome en la lava");
+            esNivelSuelo = false;
+
+        }
+    }
+
+    
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "suelo")
+    //    {
+    //        Debug.Log("DEJE DE CHOCHAR CON SUELO");
+    //        esNivelSuelo = false;
+    //    }
+    //}
+
 }
